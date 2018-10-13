0dcb9cad931c64aae711bb7a7e50adca139fdb7c
jaqueline jorge loterio <rapello.florencio@gmail.com>
2018-10-13 00:44:28 -0300

    Tratamento do CORS

--- index.php
+++ index.php
@@ -47,6 +47,25 @@ $app->add(new TokenAuthentication([
     'secure' => false
 ]));
 
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
+
+// Catch-all route to serve a 404 Not Found page if none of the routes match
+// NOTE: make sure this route is defined last
+$app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function($req, $res) {
+    $handler = $this->notFoundHandler; // handle using the default Slim page not found handler
+    return $handler($req, $res);
+});
+
 /**
  * Lista de todos os usuarios
  * @request curl -X GET http://localhost:8000/user
