821cc580c39411fe55741008614df497d6b3ae85
jaqueline jorge loterio <rapello.florencio@gmail.com>
2018-10-23 21:52:04 -0300

    CORSMiddleware adicionado

--- index.php
+++ index.php
@@ -54,24 +54,14 @@ $app->add(new TokenAuthentication([
     'secure' => false
 ]));
 
-$app->options('/{routes}', function ($request, $response, $args) {
-    return $response;
-});
-
-$app->add(function ($req, $res, $next) {
-    $response = $next($req, $res);
-    return $response
-            ->withHeader('Access-Control-Allow-Origin', '*')
-            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-});
-
-// Catch-all route to serve a 404 Not Found page if none of the routes match
-// NOTE: make sure this route is defined last
-$app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes}', function($req, $res) {
-    $handler = $this->notFoundHandler; // handle using the default Slim page not found handler
-    return $handler($req, $res);
-});
+$app->add(new Tuupola\Middleware\CorsMiddleware([
+    "origin" => ["*"],
+    "methods" => ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],    
+    "headers.allow" => ["Origin", "Content-Type", "Authorization", "Accept", "ignoreLoadingBar", "X-Requested-With", "Access-Control-Allow-Origin"],
+    "headers.expose" => [],
+    "credentials" => true,
+    "cache" => 0,   
+]));
 
 /**
  * Retornando mais informações do usuario informado pelo id
